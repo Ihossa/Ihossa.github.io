eb9a1ef0778992cc5652662c9ae7cb525f1617f5
Ihossa <43220529+Ihossa@users.noreply.github.com>
2019-10-18 13:07:36 +0300

    Диплом
    Перша корекція

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style/style.css">
+  <title>Document</title>
+</head>
+<body>
+  <header>
+    <div class="container-fluid">
+      <div class="row">
+      <div class="logo col-lg-3">
+        <img src="image/logo.png" alt="logotype">
+      </div>
+      <div class="menu col-lg-6 text-center">
+        <a href="">Про дизайн</a>
+        <a href="">Послуги</a>
+        <a href="">Відгуки</a>
+        <a href="">Консультація</a>
+      </div>
+      <div class="validation col-lg-3 text-right">
+        <button type="button" class="btn">Реєстрація</button>
+        <button type="button" class="btn">Вхід</button>
+      </div>
+      </div>
+    </div>
+  </header>
+</body>
+</html>
